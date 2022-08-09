b3b6939b61b3c2098bbd70baef7f42dcdfb0f7f3
Sziyi <3275157006@qq.com>
2022-08-09 15:59:36 +0800

    购物车页面功能及首页的扫码按钮与去结算的切换

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -1,39 +1,43 @@
 // pages/shop/shop.js
 import ShopModel from "../../model/shop"
-import {navigateTo} from "../../utils/navigate"
-import {addCart} from "../../common/cart"
+import { navigateTo } from "../../utils/navigate"
+import { addCart } from "../../common/cart"
+import Storage from "../../utils/storage"
 Page({
   /**
    * 调用轮播图接口方法
    */
-  async getBanner(){
+  async getBanner() {
     const response = await ShopModel.getShopBanner()
     this.setData({
-      bannerData : response.data
+      bannerData: response.data
     })
   },
 
   // 获取商品信息
-  async getShopCode(event){
+  async getShopCode(event) {
+    if (this.data.status) {
+      navigateTo("/pages/order/order")
+    }
     // 获取商品的条形码
     const qcode = event.detail
     // 如果商品条形码不存在,则不继续往下执行
-    if(!qcode) return 
-    try{
+    if (!qcode) return
+    try {
       // 获取商品信息
       const response = await ShopModel.getShopingInfo(qcode)
       // 如果商品信息获取失败,则不继续往下执行
-      if(!response.success) return
+      if (!response.success) return
       // 获取商品信息
       const result = response.result
       // console.log(result);
       // 获取商品的数据小于等于0 , 说明没有当前条形码的商品数据,则不继续往下执行
-      if(result.length <= 0) return
+      if (result.length <= 0) return
       // 将商品添加本地
       addCart(result[0])
       // 跳转到购物车页面
       navigateTo("/pages/cart/cart")
-    }catch(err){
+    } catch (err) {
       console.log(err)
     }
   },
@@ -42,7 +46,10 @@ Page({
    * 页面的初始数据
    */
   data: {
-    bannerData: []
+    bannerData: [],
+    cartList: [],
+    status: false,
+    count: 0
   },
 
   /**
@@ -51,7 +58,18 @@ Page({
   onLoad(options) {
     this.getBanner()
   },
-
+  // 初始化获取商品数据
+  getCartList() {
+    const cartList = Storage.get("carts")
+    const status = cartList.length > 0 ? true : false
+    const count = cartList.length
+    // console.log('cartList.length', cartList.length);
+    this.setData({
+      cartList,
+      status,
+      count
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
@@ -63,7 +81,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    this.getCartList()
   },
 
   /**
